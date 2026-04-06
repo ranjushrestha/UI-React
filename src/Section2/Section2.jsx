00d6852f17fd36f773e67fa1144c5314ab621080
Ranju Shrestha <ranjustha8844@gmail.com>
2026-04-06 19:08:17 +0545

    create note-app {two way binding}

--- src/Section2/Section2.jsx
+++ src/Section2/Section2.jsx
@@ -4,15 +4,21 @@ const Section2 = () => {
   const [name, setName] = useState("")
   const submitHandler =(e) => {
     e.preventDefault()
-    console.log("submit form",name)
+    if(name){
+      console.log("submit form",name)
+    } else {
+      console.log("enter a name")
+    }
+    
+    setName('')
   }
   return (
-    <div className='h-screen w-full bg-blue-500'>
-      <form  onSubmit={e => {
+    <div className='h-screen w-full px-10 py-10 bg-blue-500'>
+      <form className=' w-50 flex flex-col' onSubmit={e => {
         submitHandler(e)
       }}>
-        <input value={name} onChange={(e) => setName(e.target.value)} className='border border-r-2' type='text' placeholder='enter your name '/>
-        <button>submit</button>
+        <input value={name} onChange={(e) => setName(e.target.value)} className='border rounded-xl mb-5 px-8 py-5' type='text' placeholder='enter your name '/>
+        <button className='border rounded-xl'>submit</button>
       </form>
     </div>
   )
